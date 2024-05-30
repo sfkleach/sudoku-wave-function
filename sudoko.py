2d62f24313e4207c687ac8fe50c01ca99cbd712c
Stephen Leach <sfkleach@gmail.com>
2024-05-30 09:19:20 +0100

    Adding custom classes

--- sudoko.py
+++ sudoko.py
@@ -35,7 +35,8 @@ EXAMPLE_EXPECTED = """
 | 8 │ 3 │ 5 | 7 │ 4 │ 9 | 1 │ 6 │ 2 |
 +---+---+---+---+---+---+---+---+---+
 | 6 │ 2 │ 7 | 3 │ 8 │ 1 | 9 │ 5 │ 4 |
-+---+---+---+---+---+---+---+---+---+"""
++---+---+---+---+---+---+---+---+---+
+"""
 
 HARD = """
 _____2___
@@ -73,14 +74,36 @@ ________8
 _7___5___
 """
 
-def convert_to_grid(s: str) -> list[list[int]]:
+
+EXTREME_EXPECTED="""
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  3  |  5  |  7  |  6  |  1  |  8  |  9  |  2  |  4  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  8  |  9  |  6  |  4  |  5  |  2  |  1  |  3  |  7  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  4  |  2  |  1  |  7  |  9  |  3  |  5  |  8  |  6  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  7  |  4  |  5  |  1  |  8  |  9  |  3  |  6  |  2  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  2  |  1  |  8  |  5  |  3  |  6  |  4  |  7  |  9  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  6  |  3  |  9  |  2  |  7  |  4  |  8  |  1  |  5  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  5  |  8  |  3  |  9  |  2  |  7  |  6  |  4  |  1  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  9  |  6  |  2  |  3  |  4  |  1  |  7  |  5  |  8  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+|  1  |  7  |  4  |  8  |  6  |  5  |  2  |  9  |  3  |
++-----+-----+-----+-----+-----+-----+-----+-----+-----+
+"""
+
+def convert_to_grid(puzzle: str) -> list[list[int]]:
+    # print('{puzzle=}')
     return [
         [int(c) if c != '_' else 0 for c in line]
-        for line in s.splitlines()
+        for line in puzzle.splitlines()
         if line
     ]
-
-SUDOKU = convert_to_grid(EXTREME)
 
 def new_sudoku(grid: Grid) -> SetGrid:
     return [
@@ -91,72 +114,92 @@ def new_sudoku(grid: Grid) -> SetGrid:
         for row in grid
     ]
 
-def pretty(grid: SetGrid):
-    # print(grid)
-    # return
+class Sudoku:
 
-    def cellstr(x):
-        if len(x) > 5:
-            return x[0:4]+"+"
+    def __init__(self, *, puzzle: str | None = None, grid: SetGrid | None = None):
+        if puzzle is not None:
+            # print(f'{puzzle=}')
+            self._grid = new_sudoku(convert_to_grid(puzzle))
+        elif grid is not None:
+            self._grid = grid
+
+    def get(self, row: int, col: int) -> set[int]:
+        return self._grid[row][col]
+    
+    def set(self, row: int, col: int, value: int):
+        """Must be used with care, as it surgically updates the Sudoku grid."""
+        self._grid[row][col] = {value}
+
+    def pretty(self):
+
+        def cellstr(x):
+            if len(x) > 5:
+                return x[0:4]+"+"
+            else:
+                n2 = ( 5 - len(x) ) // 2
+                n1 = 5 - len(x) - n2
+                return " " * n1 + x + " " * n2
+
+        print("+-----+-----+-----+-----+-----+-----+-----+-----+-----+")
+        sep = None
+        for row in self._grid:
+            if sep is not None:
+                print(sep)
+            print("|", end="")
+            for cell in row:
+                txt = "".join(map(str, cell))
+                print(f"{cellstr(txt)}", end="|")
+            print()
+            sep = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+"
+        print("+-----+-----+-----+-----+-----+-----+-----+-----+-----+")
+
+    def find_minimum_set(self) -> tuple[int, int] | None:
+        sofar = 9
+        (i, j) = (-1, -1)
+        for r, row in enumerate(self._grid):
+            for c, col in enumerate(row):
+                L = len(col)  # Size of set.
+                if 2 <= L < sofar:
+                    sofar = L
+                    i, j = r, c
+        if i == -1:
+            return None
         else:
-            n2 = ( 5 - len(x) ) // 2
-            n1 = 5 - len(x) - n2
-            return " " * n1 + x + " " * n2
+            return (i, j)
 
-    print("+-----+-----+-----+-----+-----+-----+-----+-----+-----+")
-    sep = None
-    for row in grid:
-        if sep is not None:
-            print(sep)
-        print("|", end="")
-        for cell in row:
-            txt = "".join(map(str, cell))
-            print(f"{cellstr(txt)}", end="|")
+    def propagate_constraints(self) -> 'Sudoku':
+        return Sudoku(grid=[[Focus(self, row, col).calculate_options() for col in range(9)] for row in range(9)])
+    
+    def is_valid(self) -> bool:
+        for row in self._grid:
+            for cell in row:
+                if not cell:
+                    return False
+        return True
+
+    def simplify(self) -> Generator['Sudoku', None, None]:
+        sudoku = self
+        while sudoku.is_valid():
+            g = sudoku.propagate_constraints()
+            if g._grid == sudoku._grid:
+                yield sudoku
+                break
+            sudoku = g
+
+    def solve(self, guesses) -> Generator['Sudoku', None, None]:
+        self.pretty()
         print()
-        sep = "+-----+-----+-----+-----+-----+-----+-----+-----+-----+"
-    print("+-----+-----+-----+-----+-----+-----+-----+-----+-----+")
-
-
-def find_minimum_set(grid: SetGrid) -> tuple[int, int] | None:
-    sofar = 9
-    (i, j) = (-1, -1)
-    for r, row in enumerate(grid):
-        for c, col in enumerate(row):
-            L = len(col)  # Size of set.
-            if 2 <= L < sofar:
-                sofar = L
-                i, j = r, c
-    if i == -1:
-        return None
-    else:
-        return (i, j)
-
-
-def find_box_values(grid: Grid, row: int, col: int) -> set[int]:
-    """Find the values in the 3x3 containing box"""
-    box_row = row // 3 * 3
-    box_col = col // 3 * 3
-    elements = {
-        grid[row][col]
-        for row in range(box_row, box_row + 3)
-        for col in range(box_col, box_col + 3)
-    }
-    return elements
-
-def find_box_values(grid: SetGrid, irow: int, icol: int) -> Generator[set[int], None, None]:
-    """Find the values in the 3x3 containing box"""
-    box_row = irow // 3 * 3
-    box_col = icol // 3 * 3
-    for row in range(box_row, box_row + 3):
-        for col in range(box_col, box_col + 3):
-            if row != irow or col != icol:
-                    yield grid[row][col]
-
-def find_singleton_box_set_values(grid: SetGrid, irow: int, icol: int) -> Generator[set[int], None, None]:
-    """Find the values in the 3x3 containing box"""
-    for values in find_box_values(grid, irow, icol):
-        if len(values) == 1:
-            yield next(iter(values))
+        for sg in self.simplify():
+            if minimum := sg.find_minimum_set():
+                row, col = minimum
+                L = list(sg.get(row, col))
+                for choice in L:
+                    new_guesses = [ f"Guessing {row=}, {col=}, {choice=}", *guesses ]       
+                    print(f"Guesses: {new_guesses}")
+                    sg.set(row, col, choice)
+                    yield from sg.solve(new_guesses)
+            else:
+                yield sg
 
 def other_row_coords(row: int, col: int) -> Generator[tuple[int, int], None, None]:
     """Find the coordinates in the row, excluding the current cell"""
@@ -170,94 +213,122 @@ def other_col_coords(row: int, col: int) -> Generator[tuple[int, int], None, Non
         if r != row:
             yield r, col
 
-def other_row_values(grid: SetGrid, row: int, col: int) -> Generator[set[int], None, None]:
-    """Find the values in the row that are already taken, excluding the current cell"""
-    for r, c in other_row_coords(row, col):
-        yield grid[r][c]
+def other_box_coords(row: int, col: int) -> Generator[tuple[int, int], None, None]:
+    """Find the coordinates in the 3x3 containing box, excluding the current cell"""
+    box_row = row // 3 * 3
+    box_col = col // 3 * 3
+    for r in range(box_row, box_row + 3):
+        for c in range(box_col, box_col + 3):
+            if r != row or c != col:
+                yield r, c
 
-def other_col_values(grid: SetGrid, row: int, col: int) -> Generator[set[int], None, None]:
-    """Find the values in the column that are already taken, excluding the current cell"""
-    for r, c in other_col_coords(row, col):
-        yield grid[r][c]
+class Focus:
 
-def taken_row_values(grid: SetGrid, row: int, col: int):
-    """Find the values in the row that are already taken, excluding the current cell"""
-    for value_set in other_row_values(grid, row, col):
-        if len(value_set) == 1:
-            yield next(iter(value_set))
+    def __init__(self, sudoku: Sudoku, row: int, col: int):
+        self._sudoku = sudoku
+        self._row = row
+        self._col = col
 
-def taken_col_values(grid: SetGrid, row: int, col: int):
-    """Find the values in the column that are already taken, excluding the current cell"""
-    for value_set in other_col_values(grid, row, col):
-        if len(value_set) == 1:
-            yield next(iter(value_set))
+    def find_box_values(self) -> Generator[set[int], None, None]:
+        """Find the values in the 3x3 containing box"""
+        for r, c in other_box_coords(self._row, self._col):
+            yield self._sudoku.get(r, c)
 
-def forced_row_value(grid: SetGrid, row: int, col: int):
-    """Find the value that is forced by the other values in the row"""
-    other_values = set().union(*other_row_values(grid, row, col))
-    return grid[row][col] - other_values
+    def find_singleton_box_set_values(self) -> Generator[int, None, None]:
+        """Find the values in the 3x3 containing box"""
+        for values in self.find_box_values():
+            if len(values) == 1:
+                yield next(iter(values))
 
-def forced_col_value(grid: SetGrid, row: int, col: int):
-    """Find the value that is forced by the other values in the column"""
-    other_values = set().union(*other_col_values(grid, row, col))
-    return grid[row][col] - other_values
+    def other_row_values(self) -> Generator[set[int], None, None]:
+        """Find the values in the row that are already taken, excluding the current cell"""
+        for r, c in other_row_coords(self._row, self._col):
+            yield self._sudoku.get(r, c)
 
-def forced_box_value(grid: SetGrid, row: int, col: int):
-    """Find the value that is forced by the other values in the box"""
-    other_values = set().union(*find_box_values(grid, row, col))
-    return grid[row][col] - other_values
+    def other_col_values(self) -> Generator[set[int], None, None]:
+        """Find the values in the column that are already taken, excluding the current cell"""
+        for r, c in other_col_coords(self._row, self._col):
+            yield self._sudoku.get(r, c)
 
+    def taken_row_values(self) -> Generator[int, None, None]:
+        """Find the values in the row that are already taken, excluding the current cell"""
+        for value_set in self.other_row_values():
+            if len(value_set) == 1:
+                yield next(iter(value_set))
 
-def calculate_options(grid: SetGrid, row: int, col: int) -> set[int]:
+    def taken_col_values(self):
+        """Find the values in the column that are already taken, excluding the current cell"""
+        for value_set in self.other_col_values():
+            if len(value_set) == 1:
+                yield next(iter(value_set))
 
-    forced = None
-    
-    frv = forced_row_value(grid, row, col)
-    if len(frv) == 1:
-        if forced is None:
-            forced = frv
-        elif forced != frv:
+    def value(self):
+        return self._sudoku.get(self._row, self._col)
+
+    def forced_row_value(self):
+        """Find the value that is forced by the other values in the row"""
+        other_values = set().union(*self.other_row_values())
+        return self.value() - other_values
+
+    def forced_col_value(self):
+        """Find the value that is forced by the other values in the column"""
+        other_values = set().union(*self.other_col_values())
+        return self.value() - other_values
+
+    def forced_box_value(self):
+        """Find the value that is forced by the other values in the box"""
+        other_values = set().union(*self.find_box_values())
+        return self.value() - other_values
+
+    def calculate_options(self) -> set[int]:
+
+        forced = None
+        
+        frv = self.forced_row_value()
+        if len(frv) == 1:
+            if forced is None:
+                forced = frv
+            elif forced != frv:
+                forced = set()
+        elif len(frv) > 1:
             forced = set()
-    elif len(frv) > 1:
-        forced = set()
-    
-    fcv = forced_col_value(grid, row, col)
-    if len(fcv) == 1:
-        if forced is None:
-            forced = fcv
-        elif forced != fcv:
+        
+        fcv = self.forced_col_value()
+        if len(fcv) == 1:
+            if forced is None:
+                forced = fcv
+            elif forced != fcv:
+                forced = set()
+        elif len(fcv) > 1:
             forced = set()
-    elif len(fcv) > 1:
-        forced = set()
-    
-    fbv = forced_box_value(grid, row, col)
-    if len(fbv) == 1:
-        if forced is None:
-            forced = fbv
-        elif forced != fbv:
+        
+        fbv = self.forced_box_value()
+        if len(fbv) == 1:
+            if forced is None:
+                forced = fbv
+            elif forced != fbv:
+                forced = set()
+        elif len(fbv) > 1:
             forced = set()
-    elif len(fbv) > 1:
-        forced = set()
 
-    if forced is not None:
-        return forced
+        if forced is not None:
+            return forced
 
-    singleton_row_values = set(taken_row_values(grid, row, col))
-    singleton_col_values = set(taken_col_values(grid, row, col))
-    box_values = set(find_singleton_box_set_values(grid, row, col))
-    options = grid[row][col] - singleton_row_values - singleton_col_values - box_values
-    # print(f"{grid[row][col]}, srv={singleton_row_values}, src={singleton_col_values}, bv={box_values}")
-    # print(f"{options=}")
-    # if forced is not None and not(forced) and options:
-    #     print(f"{row=}, {col=}, value={grid[row][col]}")
-    #     print(f"{options=}, {forced=}")
-    #     print(f"{frv=}, {list(other_row_values())}")
-    #     print(f"{fcv=}, {list(other_col_values())}")
-    #     print(f"{fbv=}")
-    return options
+        singleton_row_values = set(self.taken_row_values())
+        singleton_col_values = set(self.taken_col_values())
+        box_values = set(self.find_singleton_box_set_values())
+        options = self.value() - singleton_row_values - singleton_col_values - box_values
+        # print(f"{grid[row][col]}, srv={singleton_row_values}, src={singleton_col_values}, bv={box_values}")
+        # print(f"{options=}")
+        # if forced is not None and not(forced) and options:
+        #     print(f"{row=}, {col=}, value={grid[row][col]}")
+        #     print(f"{options=}, {forced=}")
+        #     print(f"{frv=}, {list(other_row_values())}")
+        #     print(f"{fcv=}, {list(other_col_values())}")
+        #     print(f"{fbv=}")
+        return options
 
-def propagate_constraints(grid: SetGrid) -> SetGrid:
-    return [[calculate_options(grid, row, col) for col in range(9)] for row in range(9)]
+
 
 """
 THE PLAN:
@@ -269,48 +340,19 @@ THE PLAN:
 6. go again
 """
 
-def is_valid(grid: SetGrid) -> bool:
-    for row in grid:
-        for cell in row:
-            if not cell:
-                return False
-    return True
 
-def simplify(grid: SetGrid) -> Generator[SetGrid, None, None]:
-    while is_valid(grid):
-        g = propagate_constraints(grid)
-        if g == grid:
-            yield grid
-            break
-        grid = g
-
-def solve(grid: SetGrid, guesses) -> Generator[SetGrid, None, None]:
-    pretty(grid)
-    print()
-    for sg in simplify(grid):
-        if minimum := find_minimum_set(sg):
-            row, col = minimum
-            L = list(sg[row][col])
-            for choice in L:
-                new_guesses = [ f"Guessing {row=}, {col=}, {choice=}", *guesses ]       
-                print(f"Guesses: {new_guesses}")
-                sg[row][col] = {choice}
-                yield from solve(sg, new_guesses)
-        else:
-            yield sg
 
 def main(puzzle: str):
-    S = new_sudoku(convert_to_grid(puzzle))
-    
-    simplified = list(simplify(S))
+    S = Sudoku(puzzle=puzzle)
+    simplified = list(S.simplify())
     if not simplified:
         print("No solution")
         return
     
-    for n, G in enumerate(solve(simplified[0], [])):
+    for n, G in enumerate(simplified[0].solve([])):
         print(f'SOLUTION #{n+1}')
-        pretty(G)
+        G.pretty()
         break
 
 if __name__ == "__main__":
-    main(EXTREME)
+    main(EXPERT)
